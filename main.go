a0f438dffc8214c393832c103d8d7e997fe68976
Dmitry Ignatovskij <dignatovskij102@gmail.com>
2025-01-22 20:12:14 +0300

    Начало разработки

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+//Общая стркуктура для игрока и дилера
+type Entity struct {
+	health    uint8
+	alive     bool
+	inventory [8]uint8
+}
+
+func (e *Entity) get_1_damage() {
+	e.health = e.health - 1
+}
+
+func (e *Entity) get_2_damage() {
+	e.health = e.health - 2
+}
+
+//Общая структура для предметов
+type Item struct {
+	name string
+	ID   uint8
+}
+
 func main() {
 
 }
